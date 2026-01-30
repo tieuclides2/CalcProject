b1f247d204aaad492c2d8eaf793ed2a57206f11d
tieuclides2 <43557271+tieuclides2@users.noreply.github.com>
2026-01-30 14:44:07 -0300

    Update FormPrincipalCalc.pas

--- FormPrincipalCalc.pas
+++ FormPrincipalCalc.pas
@@ -74,7 +74,7 @@ begin
   Result := '';
   for I := 1 to Length(aValue) do
     if aValue[I] in ['0' .. '9'] then
-      Result := Result + aValue[I] + 'AAAA';
+      Result := Result + aValue[I];
 end;
 
 function TForm1.Somar(aNumber1, aNumber2: Integer): Integer;
@@ -92,4 +92,5 @@ begin
 end;
 
 end.
+
 
